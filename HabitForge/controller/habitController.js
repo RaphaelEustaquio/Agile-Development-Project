a6d265f4bb482f415db0076ba3a6ede4d05ec75d
Raph <raffy211@gmail.com>
2023-04-28 21:12:47 -0700

    changed the addHabit function to include an ID to each added habit as well as added an editHabit function

--- HabitForge/controller/habitController.js
+++ HabitForge/controller/habitController.js
@@ -4,7 +4,10 @@ const users = require('../data/users.json');
 const { checkAuthenticated } = require('../middleware/authMiddleware');
 
 const addHabit = (req, res) => {
-    const habit = req.body.habit;
+    const habit = {
+        id: Date.now().toString(),
+        name: req.body.habit,
+    };
     const user = users.find((user) => user.id === req.user.id);
 
     if (user) {
@@ -15,4 +18,13 @@ const addHabit = (req, res) => {
     res.redirect('/');
 };
 
-module.exports = { addHabit };
+const editHabit = (req, res) => {
+    const habit = req.user.habits.find((h) => h.id === req.params.habitId);
+    if (!habit) {
+      return res.redirect('/');
+    }
+    res.render('edit-habit.ejs', { user: req.user, habit });
+  };
+  
+
+module.exports = { addHabit, editHabit };
