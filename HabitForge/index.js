a9e10621a90a1ae4fd97651b0ef5019c0432acc0
Raph <raffy211@gmail.com>
2023-04-28 23:46:46 -0700

    separated the css from the inline styling that was done previously inside the ejs files. There is now a public directory with a style.css file within that will take care of all the styling in the future. Linked the application to go to that style.css file by adding a line of code inside index.js

--- HabitForge/index.js
+++ HabitForge/index.js
@@ -25,7 +25,7 @@ app.use(session({
     secret: 'secret',
     resave: false,
     saveUninitialized: false,
-    cookie: { maxAge: 24 * 60 * 60 * 1000 } // 24 hours
+    cookie: { maxAge: 24 * 60 * 60 * 1000 }
 }));
 app.use(flash());
 app.use(passport.initialize());
