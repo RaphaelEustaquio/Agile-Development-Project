9c9da9e0009d0e2fe17c560a6d8f048f0919ff3b
Raph <raffy211@gmail.com>
2023-05-04 15:38:47 -0700

    added the set current page function for the leaderboard page to set its proper name

--- HabitForge/routes/leaderboardRoutes.js
+++ HabitForge/routes/leaderboardRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const leaderboardController = require('../controller/leaderboardController');
 const { checkAuthenticated } = require('../middleware/authMiddleware');
+const { setCurrentPage } = require('../middleware/setCurrentPageMiddleware');
 
 // router.post('/add-habit', checkAuthenticated, habitController.addHabit); nemoone
 
-router.get("/leaderboard", checkAuthenticated, leaderboardController.list)
+router.get("/leaderboard", checkAuthenticated, setCurrentPage('leaderboard'), leaderboardController.list)
 
 router.post("/leaderboard", checkAuthenticated, leaderboardController.findAndAddFriend)
 
