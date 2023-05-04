36a0d65cf9fba2cdd02063900acee20027159d67
Raph <raffy211@gmail.com>
2023-05-04 15:37:02 -0700

    added the set current page function for the friends page to set the proper name

--- HabitForge/routes/friendRoutes.js
+++ HabitForge/routes/friendRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const friendController = require('../controller/friendController');
 const { checkAuthenticated } = require('../middleware/authMiddleware');
+const { setCurrentPage } = require('../middleware/setCurrentPageMiddleware');
 
-router.get('/friends/index', checkAuthenticated, friendController.renderFriendsIndex);
+router.get('/friends/index', checkAuthenticated, setCurrentPage('friends'), friendController.renderFriendsIndex);
 
 // Add more friend-related routes here
 
