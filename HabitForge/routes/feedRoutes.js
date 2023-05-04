d8df5211855972969e85950b5c8dd86bb14fa5f1
Raph <raffy211@gmail.com>
2023-05-04 15:37:45 -0700

    added the set current page function for the feed page to set its proper name

--- HabitForge/routes/feedRoutes.js
+++ HabitForge/routes/feedRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const feedController = require('../controller/feedController');
 const { checkAuthenticated } = require('../middleware/authMiddleware');
+const { setCurrentPage } = require('../middleware/setCurrentPageMiddleware');
 
 // router.post('/add-habit', checkAuthenticated, habitController.addHabit); nemoone
 
-router.get("/feed", checkAuthenticated, feedController.list)
+router.get("/feed", checkAuthenticated, setCurrentPage('feed'), feedController.list)
 
 // router.post("/feed", checkAuthenticated, feedRoutes.findAndAddFriend)
 
