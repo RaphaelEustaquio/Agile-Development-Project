a4c571b734016a3b2eea8fda22d5b03609307daa
Veshpa <veshpaa@gmail.com>
2023-05-06 21:53:01 -0700

    added the render add friend page function to the friend controller

--- HabitForge/controller/friendController.js
+++ HabitForge/controller/friendController.js
@@ -1,8 +1,13 @@
 const renderFriendsIndex = (req, res) => {
     res.render('friends/index.ejs', { user: req.user });
   };
-  
+
+const renderAddFriend = (req, res) => {
+  res.render('friends/add-friend.ejs', { user: req.user });
+};
+
   module.exports = {
     renderFriendsIndex,
+    renderAddFriend,
   };
   
