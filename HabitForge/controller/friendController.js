9ae8cd1c12b6971318df6d2d46cc8198875b5f58
Veshpa <veshpaa@gmail.com>
2023-05-06 21:57:16 -0700

    added a new function in the friend controller that will allow the user to search for other users

--- HabitForge/controller/friendController.js
+++ HabitForge/controller/friendController.js
@@ -6,8 +6,15 @@ const renderAddFriend = (req, res) => {
   res.render('friends/add-friend.ejs', { user: req.user });
 };
 
+const searchUsers = (req, res) => {
+  const searchQuery = req.body.search.toLowerCase();
+  const matchedUsers = users.filter(user => user.name.toLowerCase().includes(searchQuery));
+  res.render('friends/add-friend.ejs', { user: req.user, searchResults: matchedUsers });
+};
+
   module.exports = {
     renderFriendsIndex,
     renderAddFriend,
+    searchUsers,
   };
   
