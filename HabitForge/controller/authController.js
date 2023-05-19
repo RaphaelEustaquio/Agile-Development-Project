8b7f1e3dc4b55755e5bd2d15ee08660f2f77b2c9
Veshpa <veshpaa@gmail.com>
2023-05-19 15:41:29 -0700

    first attempt at changing the auth controller into working with the prisma database

--- HabitForge/controller/authController.js
+++ HabitForge/controller/authController.js
@@ -1,42 +1,39 @@
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
-const fs = require('fs');
-const path = require('path');
-const users = require('../data/users.json');
-const trees = require('../data/trees.json');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 const habitController = require('../controller/habitController.js'); 
 
-const renderIndex = (req, res) => {
+const renderIndex = async (req, res) => {
   if (req.user) {
-    habitController.checkMissedHabits(req.user);
-    habitController.updateUserPoints(req.user, 0);
-    habitController.saveUsers();
+    await habitController.checkMissedHabits(req.user);
+    await habitController.updateUserPoints(req.user, 0);
   }
+  const trees = await prisma.tree.findMany();
   res.render('userhome/index.ejs', { user: req.user, levelingThresholds: habitController.levelingThresholds, trees: trees });
 };
-
 
 const renderLogin = (req, res) => {
     res.render('auth/login.ejs');
 };
 
 const loginUser = (req, res, next) => {
-    passport.authenticate('local', (err, user, info) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.render('auth/login.ejs', { errorMessage: info.message });
+    }
+    req.logIn(user, async (err) => {
       if (err) {
         return next(err);
       }
-      if (!user) {
-        return res.render('auth/login.ejs', { errorMessage: info.message });
-      }
-      req.logIn(user, (err) => {
-        if (err) {
-          return next(err);
-        }
-        habitController.checkMissedHabits(user);
-        return res.redirect('/');
-      });
-    })(req, res, next);
-  };
+      await habitController.checkMissedHabits(user);
+      return res.redirect('/');
+    });
+  })(req, res, next);
+};
 
 const renderRegister = (req, res) => {
     res.render('auth/register.ejs');
@@ -45,22 +42,21 @@ const renderRegister = (req, res) => {
 const registerUser = async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const newUser = {
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword,
-            habits: [],
-            friends: [],
-            realfriends: [],
-            level: 1,
-            points: 0,
-            remainingPoints: 0,
-            feed: [],
-            unseen:[],
-        };
-        users.push(newUser);
-        fs.writeFileSync(path.join(__dirname, '..', 'data', 'users.json'), JSON.stringify(users, null, 2));
+        const newUser = await prisma.user.create({
+            data: {
+                name: req.body.name,
+                email: req.body.email,
+                password: hashedPassword,
+                habits: [],
+                friends: [],
+                realfriends: [],
+                level: 1,
+                points: 0,
+                remainingPoints: 0,
+                feed: [],
+                unseen:[],
+            },
+        });
         res.redirect('/login');
     } catch {
         res.redirect('/register');
