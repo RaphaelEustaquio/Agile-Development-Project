e2a5f32d85fc28cea873c89f235eb4f67c93ff41
Veshpa <veshpaa@gmail.com>
2023-05-19 19:16:39 -0700

    first attempt at changing the friend controller into working with the prisma database

--- HabitForge/controller/friendController.js
+++ HabitForge/controller/friendController.js
@@ -1,22 +1,12 @@
-const users = require('../data/users.json');
-const trees = require('../data/trees.json');
-const habitController = require('./habitController.js')
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-const renderFriendsIndex = (req, res) => {
-  const friends = [];
-  req.user.realfriends.forEach(friend => {
-    const friendUser = users.find(user => user.id === friend.id);
-    if (friendUser) {
-      friends.push({
-        id: friendUser.id,
-        name: friendUser.name,
-        email: friendUser.email,
-        level: friendUser.level,
-        points: friendUser.points,
-      });
-    }
+const renderFriendsIndex = async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: { id: req.user.id },
+    include: { realfriends: true },
   });
-  res.render('friends/index.ejs', { user: req.user, friends: friends, trees: trees });
+  res.render('friends/index.ejs', { user: req.user, friends: user.realfriends, trees: trees });
 };
 
 const renderAddFriend = (req, res) => {
@@ -27,89 +17,100 @@ const renderFriendRequests = (req, res) => {
   res.render('friends/friend-requests.ejs', { user: req.user });
 };
 
-const renderFriendHabits = (req, res) => {
+const renderFriendHabits = async (req, res) => {
   const userId = req.params.id;
-  const userToFollow = users.find(user => user.id === userId);
+  const userToFollow = await prisma.user.findUnique({ where: { id: userId } });
   res.render('friends/friend-habits.ejs', { user: userToFollow,  levelingThresholds: habitController.levelingThresholds, trees: trees });
 };
 
-
-const searchUsers = (req, res) => {
+const searchUsers = async (req, res) => {
   const searchQuery = req.body.search.toLowerCase();
-  if (!searchQuery || searchQuery === " ") {
-    return res.render('friends/add-friend.ejs', { user: req.user, searchResults: []});
-  }
-  const matchedUsers = users.filter(user => user.name.toLowerCase().includes(searchQuery) &&
-  user.name !== req.user.name && 
-  !req.user.realfriends.some(friend => friend.name === user.name));
+  const matchedUsers = await prisma.user.findMany({
+    where: {
+      AND: [
+        { name: { contains: searchQuery, mode: 'insensitive' } },
+        { name: { not: req.user.name } },
+        { realfriends: { none: { name: req.user.name } } },
+      ],
+    },
+  });
   res.render('friends/add-friend.ejs', { user: req.user, searchResults: matchedUsers });
 };
 
-const followUser = (req, res) => {
+const followUser = async (req, res) => {
   const userId = req.params.id;
-  const userToFollow = users.find(user => user.id === userId);
-
-  if (userToFollow && !req.user.friends.some(friend => friend.id === userId) && !userToFollow.friends.some(friend => friend.id === req.user.id)) {
-    userToFollow.friends.push({
-      id: req.user.id,
-      name: req.user.name,
-      email: req.user.email,
-      level: req.user.level,
-      acceptance: false,
+  const userToFollow = await prisma.user.findUnique({ where: { id: userId } });
+  if (userToFollow) {
+    await prisma.user.update({
+      where: { id: userToFollow.id },
+      data: {
+        friends: {
+          create: {
+            friendId: req.user.id,
+            name: req.user.name,
+            email: req.user.email,
+            level: req.user.level,
+            acceptance: false,
+          },
+        },
+      },
     });
-    habitController.saveUsers();
-  }  
-
+  }
   res.render('friends/add-friend.ejs', { user: req.user, searchResults: null})
 };
 
-const acceptFriend = (req, res) => {
+const acceptFriend = async (req, res) => {
   const userId = req.params.id;
-  const userToFollow = users.find(user => user.id === userId);
-  userToFollow.realfriends.push({
-    id: req.user.id,
-    name: req.user.name,
-    email:req.user.email,
-    level: req.user.level,
-    points: req.user.points,
-    acceptance: true,
-  });
-
-  req.user.realfriends.push({
-    id: userToFollow.id,
-    name: userToFollow.name,
-    email:userToFollow.email,
-    level: userToFollow.level,
-    points: userToFollow.points,
-    acceptance: true,
-  });
-
-  req.user.friends = req.user.friends.filter(friend => friend.id !== userId);
-
-  habitController.saveUsers();
-
+  const userToFollow = await prisma.user.findUnique({ where: { id: userId } });
+  if (userToFollow) {
+    const friendData = {
+      friendId: req.user.id,
+      name: req.user.name,
+      email:req.user.email,
+      level: req.user.level,
+      points: req.user.points,
+      acceptance: true,
+    };
+    await prisma.user.update({
+      where: { id: userToFollow.id },
+      data: {
+        realfriends: { create: friendData },
+        friends: { disconnect: { id: req.user.id } },
+      },
+    });
+    await prisma.user.update({
+      where: { id: req.user.id },
+      data: {
+        realfriends: { create: friendData },
+      },
+    });
+  }
   res.redirect('/friends/index');
 };
 
-
-const removeFriend = (req, res) => {
+const removeFriend = async (req, res) => {
   const userId = req.params.id;
-  const userToFollow = users.find(user => user.id === userId);
-  userToFollow.realfriends = userToFollow.realfriends.filter(friend => friend.id !== req.user.id);
-  req.user.realfriends = req.user.realfriends.filter(friend => friend.id !== userToFollow.id);
-  habitController.saveUsers();
-
+  const userToFollow = await prisma.user.findUnique({ where: { id: userId } });
+  if (userToFollow) {
+    await prisma.user.update({
+      where: { id: req.user.id },
+      data: { realfriends: { disconnect: { id: userId } } },
+    });
+    await prisma.user.update({
+      where: { id: userToFollow.id },
+      data: { realfriends: { disconnect: { id: req.user.id } } },
+    });
+  }
   res.redirect('/friends/index');
 };
 
-  module.exports = {
-    renderFriendsIndex,
-    renderAddFriend,
-    searchUsers,
-    followUser,
-    acceptFriend,
-    renderFriendRequests,
-    renderFriendHabits,
-    removeFriend
-  };
-  
+module.exports = {
+  renderFriendsIndex,
+  renderAddFriend,
+  searchUsers,
+  followUser,
+  acceptFriend,
+  renderFriendRequests,
+  renderFriendHabits,
+  removeFriend
+};
