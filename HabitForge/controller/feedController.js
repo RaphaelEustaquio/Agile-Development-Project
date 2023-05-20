c0fa6671546ec827ee2d3a00074cceabc8b1cce0
Veshpa <veshpaa@gmail.com>
2023-05-19 19:10:59 -0700

    first attempt at changing the feed controller into working with the prisma database

--- HabitForge/controller/feedController.js
+++ HabitForge/controller/feedController.js
@@ -1,16 +1,42 @@
-let users = require('../data/users.json');
-const { saveUsers } = require('./habitController.js')
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-const renderFeed = (req, res) => {
-    const user = users.find((user) => user.id === req.user.id);
-    // Get all the unseen feed items for the user
-    const unseenItems = user.feed.filter(item => user.unseen.includes(item.id));
-    // Update the user's unseen array to remove the items that are about to be rendered
-    user.unseen = user.unseen.filter(id => !unseenItems.some(item => item.id === id));
-    // Save the updated users data
-    saveUsers();
-    res.render('feed/index.ejs', { user: user, feedItems: unseenItems });
-}
+const renderFeed = async (req, res) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: req.user.id
+            },
+            include: {
+                feed: true,
+                unseen: true
+            }
+        });
 
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+
+        const unseenItems = user.feed.filter(item => user.unseen.map(u => u.id).includes(item.id));
+
+        const seenItemIds = unseenItems.map(item => item.id);
+
+        // Remove the unseen feed items from user's unseen array
+        await prisma.user.update({
+            where: { id: user.id },
+            data: {
+                unseen: {
+                    disconnect: seenItemIds.map(id => ({ id }))
+                }
+            }
+        });
+
+        res.render('feed/index.ejs', { user: user, feedItems: unseenItems });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+};
 
 module.exports = { renderFeed };
